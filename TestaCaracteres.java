f1385bec62728e4fec37b05745262f45b5cb5c73
Carlos André Alves <105798961+carlosalvez@users.noreply.github.com>
2023-03-08 21:53:28 -0300

    Completando

--- TestaCaracteres.java
+++ TestaCaracteres.java
@@ -3,9 +3,26 @@ public class TestaCaracteres {
 	
 	public static void main(String[] args) {
 		
+		char letra = 'a';
+		
+		System.out.println(letra);
+		
+		char valor = 65;
+		
+		System.out.println(valor);
+		
+		valor = (char)(valor + 1); 
+		
+		System.out.println(valor);
+		
 		String frase = "Bem vindo, Carlos André";
 	
 		System.out.println(frase);	
+		
+		frase = frase + 2020;
+				
+		System.out.println(frase);
+		
 		
 	
 	}
